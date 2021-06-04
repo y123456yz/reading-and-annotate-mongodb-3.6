6cab50c8bb49510f35d9aef0a007a5ef29a55710
y123456yz <3058078489@qq.com>
2021-06-04 14:42:19 +0800

    find cmd anzlye

--- mongo/src/mongo/db/commands/find_cmd.cpp
+++ mongo/src/mongo/db/commands/find_cmd.cpp
@@ -417,6 +417,7 @@ pad: "	  13080577566-76793693218-00011035587-01443926745-80818518372", yangtest1
         // Before saving the cursor, ensure that whatever plan we established happened with the
         // expected collection version
         auto css = CollectionShardingState::get(opCtx, nss);
+		//shard version版本检查，如果mongos发送过来的shard version和期望的不一样，则返回mongos错误
         css->checkShardVersionOrThrow(opCtx);
 
         // Set up the cursor for getMore.
